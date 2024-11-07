8ea6d4fb35e46d576588aded6843ee337299ea5b
chiefsausaaa <uwusuke0@gmail.com>
2024-11-07 00:27:14 -0800

    Add files via upload

---
+++ odds.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Sep 29 23:35:51 2024
+
+@author: DELL
+"""
+
+
+number = int(input("Please enter any number: "))
+if number%1==0:
+    print ("Even")
+   
+if number == 69:
+        print ("nice")
+        
+if number == 666:
+    print ("Wtf bro")
+    
+
+
+else:
+    print ("Odd")
