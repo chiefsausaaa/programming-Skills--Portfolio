4eb48b63301b272de66281be258fd00f42ac1322
chiefsausaaa <uwusuke0@gmail.com>
2024-11-07 00:17:16 -0800

    Add files via upload

---
+++ driving license.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Sep 29 23:21:22 2024
+
+@author: DELL
+"""
+
+
+age = int(input("Please enter your age: "))
+if age >=18:
+    print ("Eligible for driving's license")
+    
+else:
+    print ("go to bed cuhh")
