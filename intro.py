cd4223a53a3451e2a0bf539cf7addec2499a1a54
chiefsausaaa <uwusuke0@gmail.com>
2024-11-07 00:17:34 -0800

    Add files via upload

---
+++ intro.py
@@ -0,0 +1,34 @@
+
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+print ("Name:rafael amin remoquillo ")
+
+print("Age: 19")
+
+print ("City: Dubai")
+
+print ("Gender: Male")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
