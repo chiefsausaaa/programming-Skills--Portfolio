42300e50e680b8772d15ba57b39de131ddf86ff2
chiefsausaaa <uwusuke0@gmail.com>
2024-11-07 00:18:42 -0800

    Add files via upload

---
+++ variable.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Nov  6 23:49:07 2024
+
+@author: DELL
+"""
+variable1 = 8
+variable2 = 10
+sum = variable1 + variable2
+
+print(sum)
+
+# i just put a random number as a variable and added plus to the two variable
